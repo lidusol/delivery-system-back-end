a3bb64d6d48d208f7b881be6bdd45fd1389a8cab
Lydia <lydia@zagoal.com>
2020-11-12 11:20:44 +0300

    feat: change local database to cloud

--- config/database.js
+++ config/database.js
@@ -1,4 +1,8 @@
+const uri =
+  "mongodb://Helm:Helm%402020@delivery-system-shard-00-00.fjlge.mongodb.net:27017,delivery-system-shard-00-01.fjlge.mongodb.net:27017,delivery-system-shard-00-02.fjlge.mongodb.net:27017/deliverysystem?ssl=true&replicaSet=atlas-uvbgrx-shard-0&authSource=admin&retryWrites=true&w=majority";
+
 module.exports = {
   'secret': 'secret',
-  'database': 'mongodb://127.0.0.1/deliverysystem'
+  // 'database': 'mongodb://127.0.0.1/deliverysystem'
+  'database': uri
 };
