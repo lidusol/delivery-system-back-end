e484a3ffc9239a1af837d1bfd7485b0b42d1f1c2
Lydia <lydia@zagoal.com>
2020-11-12 13:10:56 +0300

    chore: update packages

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const express = require("express");
-const path = require("path");
+// const path = require("path");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const passport = require('passport');
 const mongoose = require("mongoose");
 const config = require('./config/database');
-
 const port = 3000;
 const app = express();
 
 const users = require('./routes/users');
-const items = require('./routes/items');
+const orders = require('./routes/orders');
 
-mongoose.connect(config.database);
+// mongoose.connect(config.database);
+mongoose.connect(config.database, { useMongoClinet: true });
 
 mongoose.connection.once('open', () => {
   console.log("connected to database " + config.database);
@@ -22,27 +22,59 @@ mongoose.connection.on('error', (err) => {
   console.log("database error " + config.database + err);
 });
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 app.use(passport.initialize());
 app.use(passport.session());
 require('./config/passport')(passport);
 
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
 
 app.use('/users', users);
-app.use('/items', items);
+app.use('/orders', orders);
 
-app.get('/', (req, res) => {
-  res.send("Invalid Endpoint");
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
 });
+
+
+// app.get('/', (req, res) => {
+//   res.send("Invalid Endpoint");
+// });
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public/index.html'));
+// });
 
 app.listen(port, () => {
-  console.log("server started on port " + port);
+  console.log("Server started on port " + port);
 });
+
+module.exports = app;
