d63386b0230062e336ccaf8f7ebf7ae33b0925be
Lydia <lydia@zagoal.com>
2020-11-24 20:41:59 +0300

    fix: syntax error

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const passport = require('passport');
 const mongoose = require("mongoose");
 const config = require('./config/database');
-const port = process.this.eventNames.PORT || 4000;
+const port = process.env.PORT || 4000;
 const app = express();
 
 const users = require('./routes/users');
